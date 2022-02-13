f9d5c5c712af76a1837ab01d4804db274699f285
wjbad14 <wessel.badenhorst@archsoftware.co.za>
2022-02-13 11:26:03 +0200

    First Java Commit

---
+++ src/FirstPackage/FirstClass.java
@@ -0,0 +1,14 @@
+package FirstPackage;
+
+public class FirstClass {
+	/* Line 1
+	 * Line 2
+	 * Line3
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World this is Wessel"); // This is a comment
+		System.out.println("This is my second comment");
+	}
+
+}
